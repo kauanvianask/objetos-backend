92db656b51adfc3da44ca0ebfa34ee492f6dd9d0
Kauan Viana <kauanvianasoares10@gmail.com>
2026-09-25 10:50:09 -0300

    Atividade Objetos Back End

---
+++ atv-objetos.js
@@ -0,0 +1,116 @@
+let exercicio1 = "Exercício 1 – Sistema de Login";
+let usuario = {
+    login: "admin",
+    senha: 1234,
+    tipo: "Administrador"
+};
+console.log(exercicio1)
+console.log(`O login do administrador é : ${usuario.login}. E a senha é ${usuario.senha}, muito fraca. Já o tipo do usuario é: ${usuario.tipo}`)
+
+let separacao = "------------------------------------------------------------------------------------------------------------------------------------------"
+console.log(separacao)
+
+let exercicio2 = "Exercício 2 – Cadastro de Aluno";
+let aluno = {
+    nome: "Kauan",
+    curso: "Desenvolvimento de Sistemas",
+    turma: "DSi1 - SESI"
+};
+console.log(exercicio2)
+console.log(`O nome do aluno é ${aluno.nome}. O curso é ${aluno.curso}, e está na turma ${aluno.turma}`)
+console.log(separacao)
+
+
+let exercicio3 = "Exercício 3 – Professor";
+let professor = {
+    nome: "Eduardo Falabella",
+    disciplina: "Desenvolvimento de Sistemas",
+    turno: "Integral"
+};
+console.log(exercicio3)
+console.log(`O nome do professor é ${professor.nome}. A disciplina em que da aula é ${professor.disciplina}, e o turno que da aula é ${professor.turno}`)
+console.log(separacao)
+
+
+let exercicio4 = "Exercício 4 – Empresa";
+let Empresa = {
+    nome: "Nike",
+    area: "Designer",
+    cidade: "Beaverton, Oregon"
+};
+console.log(exercicio4)
+console.log(`O nome da empresa é ${Empresa.nome}. A área em que atua é ${Empresa.area}, e a cidade aonde está localizada é ${Empresa.cidade}`)
+console.log(separacao)
+
+
+let exercicio5 = "Exercício 5 – Computador";
+let Computador = {
+    marca: "Acer",
+    processador: "Intel",
+    memoria: "Kingston"
+};
+console.log(exercicio5)
+console.log(`A marca do computador é ${Computador.marca}. O processador que ele utiliza é ${Computador.processador}, e a memória que ele tem é da ${Computador.memoria}`)
+console.log(separacao)
+
+
+let exercicio6 = "Exercício 6 – Livro";
+let Livro = {
+    Titulo: "Bíblia",
+    autor: "cerca de 40 autores diferentes ao longo de um período de aproximadamente 1.500 anos, entre eles estão. reis (como Davi e Salomão), profetas (como Isaías e Jeremias), sacerdotes, pastores, pescadores e apóstolos (como Mateus, João e Paulo). ",
+    paginas: "entre 1.100 e 2.400 páginas."
+};
+console.log(exercicio6)
+console.log(`O livro que eu escolhi tem o titulo de ${Livro.Titulo}. Não existe apenas um autor e sim ${Livro.autor}, e tem ${Livro.paginas}`)
+console.log(separacao)
+
+
+let exercicio7 = "Exercício 7 – Filme";
+let Filme = {
+    nome: "Tropa de Elite",
+    genero: "Ação",
+    ano: "2007"
+};
+console.log(exercicio7)
+console.log(`O filme que eu escolhi é ${Filme.nome}. O genero dele é ${Filme.genero}, e o ano de estreia é ${Filme.ano}`)
+console.log(separacao)
+
+
+
+let exercicio8 = "Exercício 8 – Jogo";
+let Jogo = {
+    nome: "FIFA",
+    plataforma: "Multiplataforma",
+    genero: "Esportes"
+};
+console.log(exercicio8)
+console.log(`O jogo que eu escolhi é ${Jogo.nome}. Ele é um jogo ${Jogo.plataforma} aonde tem várias opções para conseguir jogar o jogo, e o genero do jogo é ${Jogo.genero}`)
+console.log(separacao)
+
+
+
+let exercicio9 = "Exercício 9 – Escola";
+let escola = {
+    nome: "SESI",
+    cidade: "Guarulhos",
+    turno: "Manhã"
+};
+console.log(exercicio9)
+console.log(`A escola em que eu estudo é o ${escola.nome}. Ela fica no municipio de ${escola.cidade}, e o turno em que estudo é no da ${escola.turno}`)
+console.log(separacao)
+
+
+let exercicio10 = "Exercício 10 – Cadastro de Usuário";
+let Cadastro = {
+    Usuario: "eduardo",
+    Senha: 1234,
+    Nome: "Eduardo Silva",
+    Email: "eduardo@email.com",
+    Nivel: "administrador",
+    Curso: "Desenvolvimento de Sistemas",
+    Turma: "DSi1 - SESI",
+    Idade: 18
+};
+console.log(exercicio9)
+console.log(`O usuario cadastrado é ${Cadastro.Usuario}, e a senha que ele utilizou é ${Cadastro.Senha}, e o usuario foi cadastrado no nome de ${Cadastro.Nome}, o email cadastrado foi ${Cadastro.Email}, e a conta está no nivel de ${Cadastro.Nivel}, o curso que a conta foi escrita é no de ${Cadastro.Curso}, e a turma que irá frequentar é a ${Cadastro.Turma}, e a idade dele é de ${Cadastro.Idade}.`)
+console.log(separacao)
